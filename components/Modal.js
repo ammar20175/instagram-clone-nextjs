beabbeb3b0d9ac311c96ae8564cc61848306fa08
ammarahmad <ammar.ahmad6410@gmail.com>
2023-11-01 11:16:59 +0500

    post creation feature added.

--- components/Modal.js
+++ components/Modal.js
@@ -3,8 +3,19 @@ import { modalState } from "@/atoms/modalAtom";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useRef, useState } from "react";
 import { CameraIcon } from "@heroicons/react/24/outline";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import {
+	addDoc,
+	collection,
+	serverTimestamp,
+	updateDoc,
+	doc,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { db, storage } from "../firebase";
 
 function Modal() {
+	const { data: session } = useSession();
 	const [open, setOpen] = useRecoilState(modalState);
 	const [selectedFile, setSelectedFile] = useState(null);
 	const [loading, setLoading] = useState(false);
@@ -23,10 +34,33 @@ function Modal() {
 		};
 	};
 
-	const uploadPost = () => {
+	const uploadPost = async () => {
 		if (loading) return;
 
 		setLoading(true);
+
+		const docRef = await addDoc(collection(db, "posts"), {
+			username: session.user.username,
+			caption: captionRef.current.value,
+			profileImg: session.user.image,
+			timestamp: serverTimestamp(),
+		});
+
+		const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+		await uploadString(imageRef, selectedFile, "data_url").then(
+			async (snapshot) => {
+				const downloadURL = await getDownloadURL(imageRef);
+
+				await updateDoc(doc(db, "posts", docRef.id), {
+					image: downloadURL,
+				});
+			}
+		);
+
+		setOpen(false);
+		setLoading(false);
+		setSelectedFile(null);
 	};
 	return (
 		<Transition.Root show={open} as={Fragment}>
@@ -112,12 +146,13 @@ function Modal() {
 
 								<div className="mt-5 sm:mt-6">
 									<button
+										onClick={uploadPost}
 										type="button"
 										className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium
 										 text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 
 										 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
 									>
-										Upload Post
+										{loading ? "Uploading..." : "Upload Post"}
 									</button>
 								</div>
 							</div>
--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = JSON.parse(process.env.FIREBASE_PUBLIC_CONFIG);
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
