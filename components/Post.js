65a5335b12a4e28edec04c6ec7e6a59008749dde
ammarahmad <ammar.ahmad6410@gmail.com>
2023-11-01 23:28:13 +0500

    like a post feature added.

--- components/Post.js
+++ components/Post.js
@@ -10,10 +10,13 @@ import { HeartIcon as HeartIconFilled } from "@heroicons/react/24/solid";
 import {
 	addDoc,
 	collection,
+	deleteDoc,
+	doc,
 	onSnapshot,
 	orderBy,
 	query,
 	serverTimestamp,
+	setDoc,
 } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -24,6 +27,8 @@ function Post({ id, username, userImg, img, caption }) {
 	const { data: session } = useSession();
 	const [comment, setComment] = useState("");
 	const [comments, setComments] = useState([]);
+	const [likes, setLikes] = useState([]);
+	const [hasLiked, setHasLiked] = useState(false);
 
 	useEffect(
 		() =>
@@ -36,6 +41,32 @@ function Post({ id, username, userImg, img, caption }) {
 			),
 		[db]
 	);
+
+	useEffect(
+		() =>
+			onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+				setLikes(snapshot.docs)
+			),
+		[db, id]
+	);
+
+	useEffect(
+		() =>
+			setHasLiked(
+				likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+			),
+		[likes]
+	);
+
+	const likePost = async () => {
+		if (hasLiked) {
+			await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+		} else {
+			await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+				username: session.user.username,
+			});
+		}
+	};
 
 	const sendComment = async (e) => {
 		e.preventDefault();
@@ -76,7 +107,14 @@ function Post({ id, username, userImg, img, caption }) {
 			{session && (
 				<div className="flex justify-between px-4 pt-4">
 					<div className="flex space-x-4">
-						<HeartIcon className="btn" />
+						{hasLiked ? (
+							<HeartIconFilled
+								onClick={likePost}
+								className="btn text-red-500"
+							/>
+						) : (
+							<HeartIcon onClick={likePost} className="btn" />
+						)}
 						<ChatBubbleLeftEllipsisIcon className="btn" />
 						<PaperAirplaneIcon className="btn h-6 -rotate-45" />
 					</div>
@@ -87,6 +125,10 @@ function Post({ id, username, userImg, img, caption }) {
 
 			{/* caption */}
 			<p className="truncate p-5">
+				{likes.length > 0 && (
+					<p className="font-bold mb-1">{likes.length} likes</p>
+				)}
+
 				<span className="font-bold mr-1">{username} </span>
 				{caption}
 			</p>
