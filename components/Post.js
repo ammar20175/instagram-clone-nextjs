6528d63ce3adc9cf2e48e530b73086e6bfb47f75
ammarahmad <ammar.ahmad6410@gmail.com>
2023-11-01 22:50:31 +0500

    comments added and fetched.

--- components/Post.js
+++ components/Post.js
@@ -6,10 +6,51 @@ import {
 	PaperAirplaneIcon,
 	ChatBubbleLeftEllipsisIcon,
 } from "@heroicons/react/24/outline";
-
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/24/solid";
+import {
+	addDoc,
+	collection,
+	onSnapshot,
+	orderBy,
+	query,
+	serverTimestamp,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import Moment from "react-moment";
 
 function Post({ id, username, userImg, img, caption }) {
+	const { data: session } = useSession();
+	const [comment, setComment] = useState("");
+	const [comments, setComments] = useState([]);
+
+	useEffect(
+		() =>
+			onSnapshot(
+				query(
+					collection(db, "posts", id, "comments"),
+					orderBy("timestamp", "desc")
+				),
+				(snapshot) => setComments(snapshot.docs)
+			),
+		[db]
+	);
+
+	const sendComment = async (e) => {
+		e.preventDefault();
+
+		const commentToSend = comment;
+		setComment("");
+
+		await addDoc(collection(db, "posts", id, "comments"), {
+			comment: commentToSend,
+			username: session.user.username,
+			userImage: session.user.image,
+			timestamp: serverTimestamp(),
+		});
+	};
+
 	return (
 		<div className="bg-white my-7 border rounded-sm">
 			{/* header */}
@@ -32,15 +73,17 @@ function Post({ id, username, userImg, img, caption }) {
 			/>
 
 			{/* Buttons */}
-			<div className="flex justify-between px-4 pt-4">
-				<div className="flex space-x-4">
-					<HeartIcon className="btn" />
-					<ChatBubbleLeftEllipsisIcon className="btn" />
-					<PaperAirplaneIcon className="btn h-6 -rotate-45" />
-				</div>
+			{session && (
+				<div className="flex justify-between px-4 pt-4">
+					<div className="flex space-x-4">
+						<HeartIcon className="btn" />
+						<ChatBubbleLeftEllipsisIcon className="btn" />
+						<PaperAirplaneIcon className="btn h-6 -rotate-45" />
+					</div>
 
-				<BookmarkIcon className="btn" />
-			</div>
+					<BookmarkIcon className="btn" />
+				</div>
+			)}
 
 			{/* caption */}
 			<p className="truncate p-5">
@@ -49,18 +92,50 @@ function Post({ id, username, userImg, img, caption }) {
 			</p>
 
 			{/* comments */}
+			{comments.length > 0 && (
+				<div className="ml-10 max-h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+					{comments.map((comment) => (
+						<div key={comment.id} className="flex items-center space-x-2 mb-3">
+							<img
+								className="h-7 rounded-full"
+								src={comment.data().userImage}
+								alt="img"
+							/>
+							<p className="text-sm flex-1">
+								<span className="font-bold">{comment.data().username}</span>{" "}
+								{comment.data().comment}
+							</p>
+
+							<Moment fromNow className="pr-5 text-xs">
+								{comment.data().timestamp?.toDate()}
+							</Moment>
+						</div>
+					))}
+				</div>
+			)}
 
 			{/* input box */}
-			<form className="flex items-center p-4">
-				<FaceSmileIcon className="h-7" />
+			{session && (
+				<form className="flex items-center p-4">
+					<FaceSmileIcon className="h-7" />
 
-				<input
-					className="border-none flex-1 focus:ring-0 outline-none"
-					type="text"
-					placeholder="Add a comment..."
-				/>
-				<button className="font-semibold text-blue-400">Post</button>
-			</form>
+					<input
+						value={comment}
+						onChange={(e) => setComment(e.target.value)}
+						className="border-none flex-1 focus:ring-0 outline-none"
+						type="text"
+						placeholder="Add a comment..."
+					/>
+					<button
+						type="submit"
+						disabled={!comment.trim()}
+						onClick={sendComment}
+						className="font-semibold text-blue-400 disabled:cursor-not-allowed"
+					>
+						Post
+					</button>
+				</form>
+			)}
 		</div>
 	);
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,37 +1,32 @@
+import { useEffect, useState } from "react";
 import Post from "./Post";
-
-const img1 =
-	"https://images.pexels.com/photos/18847930/pexels-photo-18847930/free-photo-of-venice.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-const img2 =
-	"https://images.pexels.com/photos/18322030/pexels-photo-18322030/free-photo-of-cold-juicy-beverage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-const posts = [
-	{
-		id: "123",
-		username: "ammar",
-		userImg: img1,
-		img: img1,
-		caption: "Hey you know great things are comming",
-	},
-	{
-		id: "432",
-		username: "ahmad",
-		userImg: img2,
-		img: img2,
-		caption: "Hey you know great things are comming very soon",
-	},
-];
+import { db } from "../firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 
 function Posts() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(
+		() =>
+			onSnapshot(
+				query(collection(db, "posts"), orderBy("timestamp", "desc")),
+				(snapshot) => {
+					setPosts(snapshot.docs);
+				}
+			),
+		[db]
+	);
+
 	return (
 		<div>
 			{posts.map((post) => (
 				<Post
 					key={post.id}
 					id={post.id}
-					username={post.username}
-					userImg={post.userImg}
-					img={post.img}
-					caption={post.caption}
+					username={post.data().username}
+					userImg={post.data().profileImg}
+					img={post.data().image}
+					caption={post.data().caption}
 				/>
 			))}
 		</div>
